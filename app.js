c208689c0f0e105309f753cdc4da1ceec5a74962
Carrie Wladis <carriewladis@gmail.com>
2016-04-08 17:15:21 -0400

    post directory and api by numbers added

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var fs = require('fs');
 var express = require('express');
 var	app = express();
 
-// app.get('/', function(request, response) {  
-// 	response.sendFile (__dirname + '/db/books.json');
-// });
+app.set('view engine', 'ejs');
 
-app.get('/', require('./routes/all') );//including .js files by default
+app.get('/api/:index?', require('./routes/all') );
+	//by adding /api we can now access the list of books from localhost:8888/api/....
+
+app.get('/', function (req, res) {
+	allBooks = require('./db/books.json');
+	res.render('index', allBooks );
+
+});	
 
 app.listen(8888, function () {
 	console.log('Listening on port 8888!');
--- routes/all.js
+++ routes/all.js
@@ -1,3 +1,30 @@
-module.exports = function(req, res){
-	res.send("all books");		
+var allBooks = require('../db/books.json');  //THIS IS REQUIRING THE FILE FOR ALL ROUTES USING THE REQUIRE FUNCT AND ASSIGNS THE RETURN VALUE TO A VAR
+//THIS COULD BE A MAP OR FOREACH AS THIS IS ALREADY A JS OBJECT THAT WE COULD WORK WITH
+
+module.exports = function(req, res){  
+	var stuff = req.params.index;
+
+	if (typeof stuff == 'undefined' ) {
+		res.send(allBooks);		 
+	}else {
+		stuff = Number(stuff);
+
+		if ( Number.isNaN(stuff) ) { //this is how we call Nan bc it is a reference
+			res.status(404).send('We need a number!');
+		}else {	
+		    if(stuff > allBooks.length) {
+		  		res.send('That number is too big. We only have ' + allBooks.length + ' books.');
+		    }else {	
+				if(stuff < 1) {
+					stuff = 0;
+					var thisBook = allBooks[stuff];
+					res.send(thisBook);
+				}
+
+				stuff --;
+				var thisBook = allBooks[stuff];
+				res.send(thisBook);
+            }
+		}
+	}
 }
